6d834982a2a6b21be14da83fc06659be40b904ec
Joao Cicero de Lira Filho <jc_ifsp@hotmail.com>
2013-01-18 11:57:52 -0200

    Commit Inicial 18-01-13

---
+++ SystemFunipintura/CadCliente.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace SystemFunipintura
+{
+    partial class CadCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Nome = new System.Windows.Forms.TextBox();
+            this.txt_CpfCnpj = new System.Windows.Forms.TextBox();
+            this.txt_Rua = new System.Windows.Forms.TextBox();
+            this.txt_Numero = new System.Windows.Forms.TextBox();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.Label_Nome = new System.Windows.Forms.Label();
+            this.label_CpfCnpj = new System.Windows.Forms.Label();
+            this.label_Numero = new System.Windows.Forms.Label();
+            this.label_Rua = new System.Windows.Forms.Label();
+            this.label_UF = new System.Windows.Forms.Label();
+            this.label_Cidade = new System.Windows.Forms.Label();
+            this.cbx_UF = new System.Windows.Forms.ComboBox();
+            this.cbx_Cidade = new System.Windows.Forms.ComboBox();
+            this.label_Email = new System.Windows.Forms.Label();
+            this.txt_TelCelular = new System.Windows.Forms.TextBox();
+            this.label_TelCelular = new System.Windows.Forms.Label();
+            this.label_TelResidencial = new System.Windows.Forms.Label();
+            this.label_TelComercial = new System.Windows.Forms.Label();
+            this.txt_TelResidencial = new System.Windows.Forms.TextBox();
+            this.txt_TelComercial = new System.Windows.Forms.TextBox();
+            this.label_OBS = new System.Windows.Forms.Label();
+            this.txt_Observacao = new System.Windows.Forms.TextBox();
+            this.bt_Limpar = new System.Windows.Forms.Button();
+            this.bt_Gravar = new System.Windows.Forms.Button();
+            this.bt_Salvar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_Nome
+            // 
+            this.txt_Nome.Location = new System.Drawing.Point(6, 32);
+            this.txt_Nome.Name = "txt_Nome";
+            this.txt_Nome.Size = new System.Drawing.Size(619, 20);
+            this.txt_Nome.TabIndex = 0;
+            // 
+            // txt_CpfCnpj
+            // 
+            this.txt_CpfCnpj.Location = new System.Drawing.Point(631, 32);
+            this.txt_CpfCnpj.Name = "txt_CpfCnpj";
+            this.txt_CpfCnpj.Size = new System.Drawing.Size(100, 20);
+            this.txt_CpfCnpj.TabIndex = 1;
+            // 
+            // txt_Rua
+            // 
+            this.txt_Rua.Location = new System.Drawing.Point(18, 109);
+            this.txt_Rua.Name = "txt_Rua";
+            this.txt_Rua.Size = new System.Drawing.Size(617, 20);
+            this.txt_Rua.TabIndex = 2;
+            // 
+            // txt_Numero
+            // 
+            this.txt_Numero.Location = new System.Drawing.Point(641, 109);
+            this.txt_Numero.Name = "txt_Numero";
+            this.txt_Numero.Size = new System.Drawing.Size(102, 20);
+            this.txt_Numero.TabIndex = 3;
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Location = new System.Drawing.Point(392, 148);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(351, 20);
+            this.txt_Email.TabIndex = 5;
+            // 
+            // Label_Nome
+            // 
+            this.Label_Nome.AutoSize = true;
+            this.Label_Nome.Location = new System.Drawing.Point(6, 16);
+            this.Label_Nome.Name = "Label_Nome";
+            this.Label_Nome.Size = new System.Drawing.Size(35, 13);
+            this.Label_Nome.TabIndex = 8;
+            this.Label_Nome.Text = "Nome";
+            // 
+            // label_CpfCnpj
+            // 
+            this.label_CpfCnpj.AutoSize = true;
+            this.label_CpfCnpj.Location = new System.Drawing.Point(628, 16);
+            this.label_CpfCnpj.Name = "label_CpfCnpj";
+            this.label_CpfCnpj.Size = new System.Drawing.Size(59, 13);
+            this.label_CpfCnpj.TabIndex = 9;
+            this.label_CpfCnpj.Text = "CPF/CNPJ";
+            // 
+            // label_Numero
+            // 
+            this.label_Numero.AutoSize = true;
+            this.label_Numero.Location = new System.Drawing.Point(638, 93);
+            this.label_Numero.Name = "label_Numero";
+            this.label_Numero.Size = new System.Drawing.Size(44, 13);
+            this.label_Numero.TabIndex = 10;
+            this.label_Numero.Text = "Numero";
+            // 
+            // label_Rua
+            // 
+            this.label_Rua.AutoSize = true;
+            this.label_Rua.Location = new System.Drawing.Point(2, 17);
+            this.label_Rua.Name = "label_Rua";
+            this.label_Rua.Size = new System.Drawing.Size(27, 13);
+            this.label_Rua.TabIndex = 11;
+            this.label_Rua.Text = "Rua";
+            // 
+            // label_UF
+            // 
+            this.label_UF.AutoSize = true;
+            this.label_UF.Location = new System.Drawing.Point(3, 95);
+            this.label_UF.Name = "label_UF";
+            this.label_UF.Size = new System.Drawing.Size(21, 13);
+            this.label_UF.TabIndex = 12;
+            this.label_UF.Text = "UF";
+            // 
+            // label_Cidade
+            // 
+            this.label_Cidade.AutoSize = true;
+            this.label_Cidade.Location = new System.Drawing.Point(66, 171);
+            this.label_Cidade.Name = "label_Cidade";
+            this.label_Cidade.Size = new System.Drawing.Size(40, 13);
+            this.label_Cidade.TabIndex = 13;
+            this.label_Cidade.Text = "Cidade";
+            // 
+            // cbx_UF
+            // 
+            this.cbx_UF.FormattingEnabled = true;
+            this.cbx_UF.Location = new System.Drawing.Point(19, 187);
+            this.cbx_UF.Name = "cbx_UF";
+            this.cbx_UF.Size = new System.Drawing.Size(44, 21);
+            this.cbx_UF.TabIndex = 14;
+            // 
+            // cbx_Cidade
+            // 
+            this.cbx_Cidade.FormattingEnabled = true;
+            this.cbx_Cidade.Location = new System.Drawing.Point(69, 187);
+            this.cbx_Cidade.Name = "cbx_Cidade";
+            this.cbx_Cidade.Size = new System.Drawing.Size(674, 21);
+            this.cbx_Cidade.TabIndex = 15;
+            // 
+            // label_Email
+            // 
+            this.label_Email.AutoSize = true;
+            this.label_Email.Location = new System.Drawing.Point(389, 131);
+            this.label_Email.Name = "label_Email";
+            this.label_Email.Size = new System.Drawing.Size(32, 13);
+            this.label_Email.TabIndex = 16;
+            this.label_Email.Text = "Email";
+            // 
+            // txt_TelCelular
+            // 
+            this.txt_TelCelular.Location = new System.Drawing.Point(18, 148);
+            this.txt_TelCelular.Name = "txt_TelCelular";
+            this.txt_TelCelular.Size = new System.Drawing.Size(122, 20);
+            this.txt_TelCelular.TabIndex = 17;
+            // 
+            // label_TelCelular
+            // 
+            this.label_TelCelular.AutoSize = true;
+            this.label_TelCelular.Location = new System.Drawing.Point(3, 56);
+            this.label_TelCelular.Name = "label_TelCelular";
+            this.label_TelCelular.Size = new System.Drawing.Size(60, 13);
+            this.label_TelCelular.TabIndex = 18;
+            this.label_TelCelular.Text = "Tel. Celular";
+            // 
+            // label_TelResidencial
+            // 
+            this.label_TelResidencial.AutoSize = true;
+            this.label_TelResidencial.Location = new System.Drawing.Point(143, 132);
+            this.label_TelResidencial.Name = "label_TelResidencial";
+            this.label_TelResidencial.Size = new System.Drawing.Size(83, 13);
+            this.label_TelResidencial.TabIndex = 19;
+            this.label_TelResidencial.Text = "Tel. Residencial";
+            // 
+            // label_TelComercial
+            // 
+            this.label_TelComercial.AutoSize = true;
+            this.label_TelComercial.Location = new System.Drawing.Point(266, 132);
+            this.label_TelComercial.Name = "label_TelComercial";
+            this.label_TelComercial.Size = new System.Drawing.Size(74, 13);
+            this.label_TelComercial.TabIndex = 20;
+            this.label_TelComercial.Text = "Tel. Comercial";
+            // 
+            // txt_TelResidencial
+            // 
+            this.txt_TelResidencial.Location = new System.Drawing.Point(146, 148);
+            this.txt_TelResidencial.Name = "txt_TelResidencial";
+            this.txt_TelResidencial.Size = new System.Drawing.Size(117, 20);
+            this.txt_TelResidencial.TabIndex = 21;
+            // 
+            // txt_TelComercial
+            // 
+            this.txt_TelComercial.Location = new System.Drawing.Point(269, 148);
+            this.txt_TelComercial.Name = "txt_TelComercial";
+            this.txt_TelComercial.Size = new System.Drawing.Size(117, 20);
+            this.txt_TelComercial.TabIndex = 22;
+            // 
+            // label_OBS
+            // 
+            this.label_OBS.AutoSize = true;
+            this.label_OBS.Location = new System.Drawing.Point(2, 135);
+            this.label_OBS.Name = "label_OBS";
+            this.label_OBS.Size = new System.Drawing.Size(32, 13);
+            this.label_OBS.TabIndex = 23;
+            this.label_OBS.Text = "OBS:";
+            // 
+            // txt_Observacao
+            // 
+            this.txt_Observacao.Location = new System.Drawing.Point(18, 227);
+            this.txt_Observacao.Multiline = true;
+            this.txt_Observacao.Name = "txt_Observacao";
+            this.txt_Observacao.Size = new System.Drawing.Size(725, 79);
+            this.txt_Observacao.TabIndex = 24;
+            // 
+            // bt_Limpar
+            // 
+            this.bt_Limpar.Location = new System.Drawing.Point(512, 323);
+            this.bt_Limpar.Name = "bt_Limpar";
+            this.bt_Limpar.Size = new System.Drawing.Size(75, 23);
+            this.bt_Limpar.TabIndex = 25;
+            this.bt_Limpar.Text = "Limpar";
+            this.bt_Limpar.UseVisualStyleBackColor = true;
+            // 
+            // bt_Gravar
+            // 
+            this.bt_Gravar.Location = new System.Drawing.Point(593, 323);
+            this.bt_Gravar.Name = "bt_Gravar";
+            this.bt_Gravar.Size = new System.Drawing.Size(75, 23);
+            this.bt_Gravar.TabIndex = 26;
+            this.bt_Gravar.Text = "Gravar";
+            this.bt_Gravar.UseVisualStyleBackColor = true;
+            // 
+            // bt_Salvar
+            // 
+            this.bt_Salvar.Location = new System.Drawing.Point(674, 323);
+            this.bt_Salvar.Name = "bt_Salvar";
+            this.bt_Salvar.Size = new System.Drawing.Size(75, 23);
+            this.bt_Salvar.TabIndex = 27;
+            this.bt_Salvar.Text = "Salvar";
+            this.bt_Salvar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Label_Nome);
+            this.groupBox1.Controls.Add(this.txt_Nome);
+            this.groupBox1.Controls.Add(this.txt_CpfCnpj);
+            this.groupBox1.Controls.Add(this.label_CpfCnpj);
+            this.groupBox1.Location = new System.Drawing.Point(12, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(737, 60);
+            this.groupBox1.TabIndex = 28;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_UF);
+            this.groupBox2.Controls.Add(this.label_OBS);
+            this.groupBox2.Controls.Add(this.label_TelCelular);
+            this.groupBox2.Controls.Add(this.label_Rua);
+            this.groupBox2.Location = new System.Drawing.Point(13, 76);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(736, 241);
+            this.groupBox2.TabIndex = 29;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Endereço";
+            // 
+            // CadCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 357);
+            this.Controls.Add(this.bt_Salvar);
+            this.Controls.Add(this.bt_Gravar);
+            this.Controls.Add(this.bt_Limpar);
+            this.Controls.Add(this.txt_Observacao);
+            this.Controls.Add(this.txt_TelComercial);
+            this.Controls.Add(this.txt_TelResidencial);
+            this.Controls.Add(this.label_TelComercial);
+            this.Controls.Add(this.label_TelResidencial);
+            this.Controls.Add(this.txt_TelCelular);
+            this.Controls.Add(this.label_Email);
+            this.Controls.Add(this.cbx_Cidade);
+            this.Controls.Add(this.cbx_UF);
+            this.Controls.Add(this.label_Cidade);
+            this.Controls.Add(this.label_Numero);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.txt_Numero);
+            this.Controls.Add(this.txt_Rua);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "CadCliente";
+            this.Text = "Dados do Cliente";
+            this.Load += new System.EventHandler(this.CadCliente_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Nome;
+        private System.Windows.Forms.TextBox txt_CpfCnpj;
+        private System.Windows.Forms.TextBox txt_Rua;
+        private System.Windows.Forms.TextBox txt_Numero;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.Label Label_Nome;
+        private System.Windows.Forms.Label label_CpfCnpj;
+        private System.Windows.Forms.Label label_Numero;
+        private System.Windows.Forms.Label label_Rua;
+        private System.Windows.Forms.Label label_UF;
+        private System.Windows.Forms.Label label_Cidade;
+        private System.Windows.Forms.ComboBox cbx_UF;
+        private System.Windows.Forms.ComboBox cbx_Cidade;
+        private System.Windows.Forms.Label label_Email;
+        private System.Windows.Forms.TextBox txt_TelCelular;
+        private System.Windows.Forms.Label label_TelCelular;
+        private System.Windows.Forms.Label label_TelResidencial;
+        private System.Windows.Forms.Label label_TelComercial;
+        private System.Windows.Forms.TextBox txt_TelResidencial;
+        private System.Windows.Forms.TextBox txt_TelComercial;
+        private System.Windows.Forms.Label label_OBS;
+        private System.Windows.Forms.TextBox txt_Observacao;
+        private System.Windows.Forms.Button bt_Limpar;
+        private System.Windows.Forms.Button bt_Gravar;
+        private System.Windows.Forms.Button bt_Salvar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ SystemFunipintura/CadCliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemFunipintura
+{
+    public partial class CadCliente : Form
+    {
+        public CadCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void CadCliente_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
